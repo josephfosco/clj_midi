451386d6d5d5ec1113ff9c96818de6f967ddffe7
josephfosco <josephfosco@protonmail.com>
2026-04-23 16:42:26 -0500

    add running status

--- async/src/midi_async/parser.clj
+++ async/src/midi_async/parser.clj
@@ -3,42 +3,55 @@
 (defn status-byte? [b]
   (>= b 0x80))
 
-(defn message-length [status]
+(defn channel-message-length [status]
   (let [t (bit-and status 0xF0)]
     (cond
-      (or (= t 0xC0) (= t 0xD0)) 1
-      (= t 0xB0) 2
-      (= t 0x90) 2
-      (= t 0x80) 2
+      (or (= t 0xC0) (= t 0xD0)) 1   ;; program change, channel pressure
+      (= t 0xB0) 2                   ;; CC
+      (= t 0x90) 2                   ;; note on
+      (= t 0x80) 2                   ;; note off
       :else 0)))
 
 (defn make-parser []
   (let [state (atom {:status nil
                      :buffer []})]
+
     (fn [byte]
       (let [{:keys [status buffer]} @state]
+
         (cond
+          ;; 1. NEW STATUS BYTE
           (status-byte? byte)
           (do
             (reset! state {:status byte :buffer []})
             nil)
 
+          ;; 2. DATA BYTE (may use running status)
           :else
           (let [status (or status 0)
                 buf (conj buffer byte)
-                needed (message-length status)]
-            (if (= (count buf) needed)
+                needed (channel-message-length status)]
+
+            ;; ignore if we don't yet have a valid status
+            (if (zero? needed)
               (do
-                (reset! state {:status status :buffer []})
-                {:status status
-                 :data buf})
-              (do
-                (swap! state assoc :buffer buf)
-                nil))))))))
+                (reset! state {:status nil :buffer []})
+                nil)
+
+              (if (= (count buf) needed)
+                (do
+                  (reset! state {:status status :buffer []})
+                  {:status status
+                   :data buf})
+
+                (do
+                  (swap! state assoc :buffer buf)
+                  nil)))))))))
 
 (defn decode [{:keys [status data]}]
   (let [type (bit-and status 0xF0)
         ch   (bit-and status 0x0F)]
+
     (cond
       (= type 0x90)
       {:type :note-on
@@ -50,7 +63,7 @@
       {:type :note-off
        :channel ch
        :note (first data)
-       :velocity (second data)}
+       :velocity 0}
 
       (= type 0xB0)
       {:type :cc
