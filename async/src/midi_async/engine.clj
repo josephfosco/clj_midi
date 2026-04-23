d66811122735cd4e693c7c72518a2c7bcb44c5f1
josephfosco <josephfosco@protonmail.com>
2026-04-23 16:56:52 -0500

    added timestamp

--- async/src/midi_async/engine.clj
+++ async/src/midi_async/engine.clj
@@ -2,13 +2,22 @@
   (:require [clojure.core.async :as async]
             [midi-async.parser :as p]))
 
+(defonce start-time (atom nil))
+
 (defn start-engine [in-ch]
+
+  (reset! start-time (System/currentTimeMillis))
+  (println "Engine started at" @start-time)
+
   (let [parser (p/make-parser)]
 
     (async/go-loop []
       (when-let [b (async/<! in-ch)]
-        (when-let [msg (parser b)]
-          (println "MIDI EVENT:" (p/decode msg)))
 
-        (recur)))
-  ))
+        (when-let [msg (parser b)]
+          (let [event (assoc (p/decode msg)
+                             :timestamp (- (System/currentTimeMillis)
+                                           @start-time))]
+            (println "MIDI EVENT:" event)))
+
+        (recur)))))
